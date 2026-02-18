95eaba4516d0c208898c12add6e94b08d3a93fce
Felippe Menezes <unchain0@proton.me>
2026-02-18 19:53:17 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import constants as const
+
+
+def main() -> None:
+    print(f"Starting Asteroids with pygame version: {pygame.vernum}")
+    print(f"Screen width: {const.SCREEN_WIDTH}")
+    print(f"Screen height: {const.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
