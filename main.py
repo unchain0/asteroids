f2b5a2ec5f28f57c92fc2d2c963e51b65377738b
Felippe Menezes <unchain0@proton.me>
2026-02-18 20:23:23 -0300

    refactor: simplify QUIT logic
    Use `if` instead `match-case` to reduce one indentation

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from logger import log_state
 def game_loop(screen: pygame.Surface) -> None:
     while True:
         log_state()
+
         for event in pygame.event.get():
-            match event.type:
-                case pygame.QUIT:
-                    return
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill('black')
         pygame.display.flip()
 
