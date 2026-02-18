2e565dd540c26c59e8160bc931f4a237ed0cb4e5
Felippe Menezes <unchain0@proton.me>
2026-02-18 20:19:36 -0300

    feat: add game loop
    Only display black screen btw

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import pygame
 import constants as const
+from logger import log_state
+
+
+def game_loop(screen: pygame.Surface) -> None:
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            match event.type:
+                case pygame.QUIT:
+                    return
+        screen.fill('black')
+        pygame.display.flip()
 
 
 def main() -> None:
-    print(f"Starting Asteroids with pygame version: {pygame.vernum}")
-    print(f"Screen width: {const.SCREEN_WIDTH}")
-    print(f"Screen height: {const.SCREEN_HEIGHT}")
+    print(f'Starting Asteroids with pygame version: {pygame.vernum}')
+    print(f'Screen width: {const.SCREEN_WIDTH}')
+    print(f'Screen height: {const.SCREEN_HEIGHT}')
+
+    pygame.init()
+    screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+    game_loop(screen)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
