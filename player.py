a40a9cccf909468d7b0e9a48790a99c89e4673ad
Felippe Menezes <unchain0@proton.me>
2026-02-19 13:27:06 -0300

    feat: player move forward and backward

--- player.py
+++ player.py
@@ -1,6 +1,11 @@
 from circleshape import CircleShape
 import pygame
-from constants import LINE_WIDTH, PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import (
+    LINE_WIDTH,
+    PLAYER_RADIUS,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+)
 
 
 class Player(CircleShape):
@@ -32,7 +37,17 @@ class Player(CircleShape):
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+    def move(self, dt: float) -> None:
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
