c1d18746d8fc8ac3a6ae94ddbadf45a338b6514f
Felippe Menezes <unchain0@proton.me>
2026-02-19 13:15:47 -0300

    fix: move update player method to correct place

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def game_loop(screen: pygame.Surface, player: Player) -> None:
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill('black')
         player.update(dt)
+        screen.fill('black')
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
