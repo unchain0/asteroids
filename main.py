7fc84b91550c7d7017e32a618ac1947e2862f30a
Felippe Menezes <unchain0@proton.me>
2026-02-19 14:29:00 -0300

    refactor: use groups instead of objects directly

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import constants as const
 from logger import log_state
 
 
-def game_loop(screen: pygame.Surface, player: Player) -> None:
+def game_loop(
+    screen: pygame.Surface,
+    updatable: pygame.sprite.Group,
+    drawable: pygame.sprite.Group,
+) -> None:
     clock = pygame.time.Clock()
     dt = 0
 
@@ -15,9 +19,14 @@ def game_loop(screen: pygame.Surface, player: Player) -> None:
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for u in updatable:
+            u.update(dt)
+
         screen.fill('black')
-        player.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
@@ -28,12 +37,19 @@ def main() -> None:
     print(f'Screen height: {const.SCREEN_HEIGHT}')
 
     pygame.init()
-    screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
-    player = Player(
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)  # type: ignore
+
+    Player(
         const.SCREEN_WIDTH / 2,
         const.SCREEN_HEIGHT / 2,
     )
-    game_loop(screen, player)
+
+    screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+    game_loop(screen, updatable, drawable)
 
 
 if __name__ == '__main__':
