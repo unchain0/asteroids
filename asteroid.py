2287b201fb310b60ae3483b93cf26e10f884077f
Felippe Menezes <unchain0@proton.me>
2026-02-22 17:37:17 -0300

    feat: shot split asteroids

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
+from random import uniform
 from circleshape import CircleShape
 import constants as const
 import pygame
+
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -18,3 +21,24 @@ class Asteroid(CircleShape):
 
     def update(self, dt: float):
         self.position += self.velocity * dt
+
+    def split(self) -> None:
+        self.kill()
+
+        if self.radius <= const.ASTEROID_MIN_RADIUS:
+            return
+
+        log_event('asteroid_split')
+
+        random_angle = uniform(20, 50)
+        first_asteroid_movement = self.velocity.rotate(random_angle)
+        second_asteroid_movement = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - const.ASTEROID_MIN_RADIUS
+
+        first_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        second_asteroid = Asteroid(
+            self.position.x, self.position.y, new_radius
+        )
+
+        first_asteroid.velocity = first_asteroid_movement * 1.2
+        second_asteroid.velocity = second_asteroid_movement * 1.2
--- main.py
+++ main.py
@@ -33,6 +33,11 @@ def game_loop(
                 log_event('player_hit')
                 print('Game over!')
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event('asteroid_shot')
+                    asteroid.split()
+                    shot.kill()
 
         screen.fill('black')
 
